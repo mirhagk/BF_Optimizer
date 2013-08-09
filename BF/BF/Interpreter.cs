729bcd6bc6ecea71f50d7fde9e304e924fe0c46c
mirhagk <mirhagk@gmail.com>
2013-08-09 10:22:48 -0400

    Made Interpreter completely functional and added better test case functionality (along with 2 new test cases to test basic loops)

--- BF/BF/Interpreter.cs
+++ BF/BF/Interpreter.cs
@@ -8,23 +8,64 @@ namespace BF
 {
     class Interpreter
     {
+        public System.IO.TextReader Input{get;set;}
+        public System.IO.TextWriter Output { get; set; }
+        public System.IO.TextWriter Error { get; set; }
+        public int MemorySize { get; set; }
         public static string CleanCode(string code)
         {
             return string.Join("", code.Where((x) => "+-<>[],.".Contains(x)));
         }
-        string code;
-        public Interpreter(string code)
+        public Interpreter(int memSize = 30000, System.IO.TextReader input = null, System.IO.TextWriter output = null, System.IO.TextWriter error=null)
         {
-            this.code = CleanCode(code);
+            Input = input ?? Console.In;
+            Output = output ?? Console.Out;
+            Error = error ?? Console.Error;
+            MemorySize = memSize;
         }
-        public void Run()
+        public void Run(string code)
         {
+            int[] memory = new int[MemorySize];
+            int ptr = 0;
+            //code = CleanCode(code);
             Stack<int> labels = new Stack<int>();
             for (int i = 0; i < code.Length; i++)
             {
                 switch (code[i])
                 {
                     case '+':
+                        memory[ptr]++;
+                        break;
+                    case '-':
+                        memory[ptr]--;
+                        break;
+                    case '<':
+                        ptr--;
+                        break;
+                    case '>':
+                        ptr++;
+                        break;
+                    case '[':
+                        labels.Push(i);
+                        break;
+                    case ']':
+                        if (labels.Count == 0)
+                            Error.WriteLine("ERROR: ] has no matching [ at position {0}", i);
+                        if (memory[ptr] != 0)
+                            i = labels.Peek();
+                        else
+                            labels.Pop();
+                        break;
+                    case ',':
+                        memory[ptr] = Input.Read();
+                        break;
+                    case '.':
+                        Output.Write((char)memory[ptr]);
+                        break;
+                    default://the BF spec states that any non-understood characters should simply be ignored
+                        break;
+                    case '#':
+                        Error.WriteLine("DEBUG: {0}", string.Join(" ", memory.Take(5).Select((x)=>x.ToString())));
                         break;
                 }
             }
--- BF/BF/Program.cs
+++ BF/BF/Program.cs
@@ -9,19 +9,34 @@ namespace BF
 {
     class Program
     {
-        static void RunTest(string testName)
+        static bool RunTestFile(string fileName, string input, string expectedOutput)
         {
-            string code = File.ReadAllText(String.Format("tests\\{0}.bf",testName));
-            Interpreter interpret = new Interpreter(code);
-            interpret.Run();
+            string code = File.ReadAllText(String.Format("tests\\{0}.bf", fileName));
+            return RunTest(fileName, code, input, expectedOutput);
+        }
+        static bool RunTest(string testName, string code, string input, string expectedOutput)
+        {
+            var output = new StringWriter();
+            Interpreter interpret = new Interpreter(output: output);
+            if (input!="")
+                interpret.Input=new StringReader(input);
+            Console.WriteLine("Running Test Case {0}", testName);
+            interpret.Run(code);
+            bool passed = expectedOutput == output.ToString();
+            Console.WriteLine("\tExpected Output: {0}\n\tActual Output: {1}\n\tPassed: {2}", expectedOutput, output.ToString(), passed);
+            return passed;
         }
         static void TestCases()
         {
-            RunTest("hello_world");
+            RunTest("Loop_Test", "++[>,+.<-]", "ab", "bc");
+            RunTest("Dual_Loop_Test", "++[>,>+++++[<+.>-]<<-]", "ab", "bcdefcdefg");
+            RunTestFile("hello_world","","Hello World!\n");
         }
         static void Main(string[] args)
         {
             TestCases();
+            Console.WriteLine("Press any key to continue");
+            Console.ReadKey();
         }
     }
 }
