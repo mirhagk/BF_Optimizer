21f6ac16e25afc012f569de61aa31530b88aa076
mirhagk <mirhagk@gmail.com>
2013-08-09 09:26:26 -0400

    Added code to run test cases

--- BF/BF/Program.cs
+++ BF/BF/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,19 @@ namespace BF
 {
     class Program
     {
+        static void RunTest(string testName)
+        {
+            string code = File.ReadAllText(String.Format("tests\\{0}.bf",testName));
+            Interpreter interpret = new Interpreter(code);
+            interpret.Run();
+        }
+        static void TestCases()
+        {
+            RunTest("hello_world");
+        }
         static void Main(string[] args)
         {
+            TestCases();
         }
     }
 }
